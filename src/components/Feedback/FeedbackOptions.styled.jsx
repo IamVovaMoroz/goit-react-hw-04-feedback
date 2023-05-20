7ab82692dbfb5e1a6685836a871968bc649bb82c
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-05-20 20:07:44 +0200

    styled

--- src/components/Feedback/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions.styled.jsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const ButtonContainer = styled.div`
   display: flex;
-  gap:20px;
+  gap: 20px;
   margin: 10px;
   justify-content: center;
 `;
@@ -10,17 +10,18 @@ export const ButtonContainer = styled.div`
 export const Buttons = styled.button`
   padding: 15px 30px;
   font-size: 18px;
-
   text-transform: uppercase;
- 
   background-color: #3cadda;
   border: none;
   border-radius: 20px;
   cursor: pointer;
-
+  transition: transform 0.2s ease;
 
   &:hover {
     background-color: blue;
   }
-`;
 
+  &:active {
+    transform: scale(0.95);
+  }
+`;
